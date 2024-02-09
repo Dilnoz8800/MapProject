28ec4d31d357e676927d97d1117ea8ed22f224be
burovskiile7 <146484378+burovskiile7@users.noreply.github.com>
2024-02-08 17:14:16 -0800

    Update OfficeHoursPractice.java
    Update

--- src/OfficeHoursPractice.java
+++ src/OfficeHoursPractice.java
@@ -1,5 +1,9 @@
 public class OfficeHoursPractice {
     public static void main(String[] args) {
         System.out.println("hello I'exited to practice");
+
+        //update
+
+        System.out.println("Hello world");
     }
 }
