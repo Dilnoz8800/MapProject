23b85010dcbc01dc9d0cadbad16b0ace7b48ab2d
UCLA-Ile <burovskiile7@users.noreply.github.com>
2024-02-08 16:40:28 -0800

    Added I-class

---
+++ src/I_class.java
@@ -0,0 +1,13 @@
+public class I_class {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world");
+
+        //add features
+
+        // comlete this task
+
+    }
+}
