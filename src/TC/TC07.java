859500c950596a473082a1cfa24e1e948b4c3e72
UCLA-Ile <burovskiile7@users.noreply.github.com>
2024-02-08 17:29:42 -0800

    Added TC07 & TC08

---
+++ src/TC/TC07.java
@@ -0,0 +1,10 @@
+package TC;
+
+public class TC07 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("TC07");
+    }
+}
---
+++ src/TC/TC08.java
@@ -0,0 +1,8 @@
+package TC;
+
+public class TC08 {
+    public static void main(String[] args) {
+
+        System.out.println("TC08");
+    }
+}
